1cc27c766c1eaf86a6619c8870fc554c2a138353
Adamskiee <tosinomatthew@gmail.com>
2026-01-10 14:18:26 +0800

    feat(lan-discovery): implement LAN user discovery with FlatList and zeroconf service

--- mobile-app/sapot-mobile-app/app/(tabs)/index.tsx
+++ mobile-app/sapot-mobile-app/app/(tabs)/index.tsx
@@ -1,14 +1,43 @@
-import { StyleSheet } from 'react-native';
+import { FlatList, Pressable, StyleSheet } from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+import useLanUsers from "@/features/chat";
+import zeroconf from "@/features/chat/services/zeroconf-service";
+import { useEffect } from "react";
 
 export default function TabOneScreen() {
+  const { services } = useLanUsers();
+
+  useEffect(() => {
+    zeroconf.publishService();
+    zeroconf.startDiscovery();
+
+    return () => {
+      zeroconf.close();
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log(services);
+  }, [services]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <FlatList
+        data={services}
+        renderItem={({ item }) => (
+          <View>
+            <Text>
+              Name: {item.name}
+              IP Address: {item.addresses[0]}
+              Port: {item.port}
+              ID: {item.txt.id}
+            </Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.txt.id}
+      />
     </View>
   );
 }
@@ -16,16 +45,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
