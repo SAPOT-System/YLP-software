e2b19d41a266d4918ce8c02d29db8a9cfc6918e4
Adamskiee <tosinomatthew@gmail.com>
2026-01-12 15:07:41 +0800

    feat(lan-discovery): Added 5 secs interval for examining discovered peers

--- mobile-app/sapot-mobile-app/features/chat/hooks/use-peers.ts
+++ mobile-app/sapot-mobile-app/features/chat/hooks/use-peers.ts
@@ -1,9 +1,7 @@
-import { useEffect, useMemo, useState, useSyncExternalStore } from "react";
+import { useEffect, useMemo, useSyncExternalStore } from "react";
 import zeroconf from "../services/zeroconf-service";
-import { database } from "@/features/shared/database";
 import { useObservable } from "./use-observable";
-import Peer from "@/features/shared/database/model/Peer";
-import useDatabase from "@/features/shared/hooks/use-database";
+import { Peer, useDatabase, database } from "@/features/shared";
 
 const usePeers = () => {
   // Get currently discovered peers in LAN
@@ -22,25 +20,7 @@ const usePeers = () => {
 
   // Get peers with online status
   const peers = useMemo(() => {
-    const knownSet = new Set(knownPeers.map((p) => p._raw.id));
     const discoveredSet = new Set(discoveredPeers.map((p) => p.id));
-
-    const newDiscoveredPeers = discoveredPeers.filter(
-      (peer) => !knownSet.has(peer.id)
-    );
-
-    console.log("New discovered peers:", newDiscoveredPeers);
-
-    newDiscoveredPeers.forEach((newPeer) => {
-      createPeer({
-        id: newPeer.id,
-        ipAddress: newPeer.ipAddress,
-        port: newPeer.port,
-        username: newPeer.username,
-      });
-    });
-
-    console.log("Known peers:", knownPeers);
 
     return knownPeers.map((p) => {
       if (discoveredSet.has(p.id)) {
@@ -64,7 +44,32 @@ const usePeers = () => {
   }, [knownPeers, discoveredPeers]);
 
   useEffect(() => {
-    console.log("Peers:", peers);
+    // Examine every discovered peers if stored in database or not
+    const run = () => {
+      const knownSet = new Set(knownPeers.map((p) => p._raw.id));
+      const newDiscoveredPeers = discoveredPeers.filter(
+        (peer) => !knownSet.has(peer.id)
+      );
+      newDiscoveredPeers.forEach((newPeer) => {
+        createPeer({
+          id: newPeer.id,
+          ipAddress: newPeer.ipAddress,
+          port: newPeer.port,
+          username: newPeer.username,
+        });
+      });
+    };
+
+    run();
+
+    // Run every 5 seconds
+    const interval = setInterval(run, 5000);
+
+    return () => clearInterval(interval);
+  }, [knownPeers, discoveredPeers]);
+
+  useEffect(() => {
+    console.log("[usePeers]: Peers:", peers);
   }, [peers]);
 
   const getServices = () => {
