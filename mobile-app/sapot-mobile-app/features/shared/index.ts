d87d1ea53369cc732bffef8137f3bc1472f98df5
Adamskiee <tosinomatthew@gmail.com>
2026-01-12 15:06:50 +0800

    feat(lan-discovery): export database and Peer model in shared index

--- mobile-app/sapot-mobile-app/features/shared/index.ts
+++ mobile-app/sapot-mobile-app/features/shared/index.ts
@@ -1,1 +1,4 @@
 export { default as getUserUUID } from "./utils/get-user-uuid";
+export { default as useDatabase } from "./hooks/use-database";
+export { database } from "./database";
+export { default as Peer } from "./database/model/Peer";
