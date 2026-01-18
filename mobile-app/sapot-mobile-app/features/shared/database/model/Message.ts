f42f3d161d05a87bc078523285e858c5a5a07070
Adamskiee <tosinomatthew@gmail.com>
2026-01-18 18:48:32 +0800

    feat(chat-webrtc): add Chat, Message, and Participant models for chat features

--- mobile-app/sapot-mobile-app/features/shared/database/model/Message.ts
+++ mobile-app/sapot-mobile-app/features/shared/database/model/Message.ts
@@ -1,10 +1,18 @@
 import { Model } from "@nozbe/watermelondb";
 import { date, field, relation } from "@nozbe/watermelondb/decorators";
 
+export enum Status {
+  SENT = "sent",
+  DELIVERED = "delivered",
+  READ = "read",
+}
 export default class Message extends Model {
   static table = "messages";
 
-  @relation("peers", "peer_id") peer_id!: string;
   @field("message") message!: string;
-  @date("timestamp") timestamp!: Date;
+  @field("status") status!: Status;
+  @date("created_at") createdAt!: Date;
+
+  @relation("chats", "chat_id") chatId!: string;
+  @relation("peers", "sender_id") senderId!: string;
 }
