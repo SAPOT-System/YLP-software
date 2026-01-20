91435dd96e41c3f98e0f1805a2fc13b6731a3aac
Adamskiee <tosinomatthew@gmail.com>
2026-01-20 15:07:48 +0800

    feat(chat-webrtc): add MessageStatus model and repository for managing message statuses

---
+++ mobile-app/sapot-mobile-app/features/shared/database/model/MessageStatus.ts
@@ -0,0 +1,21 @@
+import { Model, Relation } from "@nozbe/watermelondb";
+import { field, relation } from "@nozbe/watermelondb/decorators";
+import Peer from "./Peer";
+import Message from "./Message";
+
+// TODO: replace the name for better understanding
+export enum MessageStatusType {
+  SENT = "sent",
+  DELIVERED = "delivered",
+  READ = "read",
+}
+
+export default class MessageStatus extends Model {
+  static table = "message_status";
+
+  @field("status") status!: MessageStatusType;
+
+  @relation("messages", "message")
+  message!: Relation<Message>;
+  @relation("peers", "user") user!: Relation<Peer>;
+}
