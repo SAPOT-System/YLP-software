028280919f9fd68dcf2a3ea062bf1bbc5ef3a6c6
Adamskiee <tosinomatthew@gmail.com>
2026-01-26 12:15:55 +0800

    feat(getting-started): add initial layout and mode selection screen with navigation

--- mobile-app/sapot-mobile-app/app/_layout.tsx
+++ mobile-app/sapot-mobile-app/app/_layout.tsx
@@ -69,6 +69,7 @@ function RootLayoutNav() {
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="getting-started" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: "modal" }} />
       </Stack>
