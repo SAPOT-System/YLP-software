534ed4e8dc42123ce67ef2f4c91e58cc8eba69ce
Emman-pip <parrenoemmanuel756@gmail.com>
2026-01-29 18:06:39 +0800

    chore(api-entry-point) Created main.py.
    Created the entry point for the fastAPI application.

---
+++ server/app/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from contextlib import asynccontextmanager
+from typing import Union
+
+from fastapi import FastAPI
+from starlette.responses import JSONResponse
+
+from app.db_operations.auth import create_db_and_tables
+from .api import auth
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+app = FastAPI(
+    title="SAPOT Server",
+    description="A server that will mitigate the backend of the SAPOT mobile application",
+    summary="A server that contains secure endpoint for the use of a mobile application",
+    version="0.0.1",
+    lifespan=lifespan
+)
+
+app.include_router(auth.router)
+
+@app.get("/")
+def read_root():
+    return {"state": "running"}
