21065944d7a64e6866fdf664626d9b6ec230c847
Emman-pip <parrenoemmanuel756@gmail.com>
2026-01-29 20:49:19 +0800

    feature(api-auth) Create tests for create account endpoint.
    The test encompasses the create accounts with IDs and without IDs.

--- server/app/tests/test_integration_auth.py
+++ server/app/tests/test_integration_auth.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+import uuid
 from fastapi.testclient import TestClient
 from fastapi.testclient import TestClient
 
-from app.tests.assets import sample_users
+from app.tests.assets import sample_users, dummy_data
+from app.db_operations.auth import SessionDep, db_create_user
+from app.models.users import UserCreate
 
 
 def test_auth_status(client:TestClient):
@@ -25,3 +28,37 @@ def test_auth_token(client:TestClient):
 
     assert "access_token" in token
     assert token['token_type'] == 'bearer'
+
+
+sample_user = {
+        'name':"emmanuel",
+        'email':"Emmanuel@gmail.com",
+        'phone_number':"093985984598",
+        'password':"hi world"
+}
+
+def test_auth_create_account(client: TestClient):
+    response = client.post('/auth', json=sample_user)
+
+    assert response.status_code == 201
+
+    response_data = response.json()
+    assert response_data.get('name') == sample_user.get('name')
+    assert response_data.get('email') == sample_user.get('email')
+    assert response_data.get('phone_number') == sample_user.get('phone_number')
+
+
+def test_auth_create_account_with_id(client: TestClient):
+    id = str(uuid.uuid4())
+    response = client.post('/auth', json={
+        **sample_user,
+        'id': id
+    })
+
+    assert response.status_code == 201
+
+    response_data = response.json()
+    assert response_data.get('id') == id
+    assert response_data.get('name') == sample_user.get('name')
+    assert response_data.get('email') == sample_user.get('email')
+    assert response_data.get('phone_number') == sample_user.get('phone_number')
