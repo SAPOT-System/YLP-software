7d124708b1618fe3248185527a7bc4562930da37
Emman-pip <parrenoemmanuel756@gmail.com>
2026-02-05 15:20:43 +0800

    feature(api-auth) Create the test for db user information update.

--- server/app/tests/test_db_auth.py
+++ server/app/tests/test_db_auth.py
@@ -12,6 +12,8 @@ from fastapi.testclient import TestClient
 from app.db_operations.auth import SessionDep, db_create_user, get_password_hash, get_session, get_user_by_email, get_user_by_phone_number, get_user_by_username, verify_password
 from app.models.users import User, UserCreate
 from app.tests.assets import dummy_data, sample_users
+from app.db_operations.auth import update_user_info
+from app.models.users import UserUpdate
 
 def test_db_create_user_with_id(session: SessionDep):
     user = db_create_user(
@@ -65,3 +67,20 @@ def test_db_get_users_by_phone_number(session : SessionDep):
     assert user.phone_number == user_data.get('phone_number')
     assert user.email == user_data.get('email')
     assert verify_password(str(user_data.get('password')), user.hashed_password)
+
+
+def test_db_change_info(session : SessionDep):
+    user_data = sample_users['test']
+    new_username = 'testing_new_username'
+
+    user = get_user_by_username(session, str(user_data.get('username')))
+    print("USERR", user)
+    update_model = UserUpdate(
+        username=new_username
+    )
+    update_user_info(user, update_model, session)
+
+    # test if the update occured
+    user = get_user_by_username(session, new_username)
+    assert user
+    assert user.username == new_username
