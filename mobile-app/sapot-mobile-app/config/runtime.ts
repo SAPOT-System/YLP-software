d88b6b0c573e322782a9e9b5b9f7cce230c264b9
Adamskiee <tosinomatthew@gmail.com>
2026-02-16 12:09:46 +0800

    feat(auth): update getApiUrl function to improve development environment handling

--- mobile-app/sapot-mobile-app/config/runtime.ts
+++ mobile-app/sapot-mobile-app/config/runtime.ts
@@ -9,6 +9,11 @@ type CustomManifest = Updates.Manifest & {
 };
 
 export const getApiUrl = () => {
+  if (process.env.NODE_ENV === "development") {
+    return "http://10.0.2.2:8000";
+  }
+
+  // TODO: needs build to work
   const manifest = Updates.manifest as CustomManifest;
   return manifest?.extra?.apiUrl;
 };
