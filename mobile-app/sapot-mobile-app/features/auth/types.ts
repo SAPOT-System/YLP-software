b2a3798d1bdeb65eb4723c93a6709b456444b994
Adamskiee <tosinomatthew@gmail.com>
2026-02-22 14:33:22 +0800

    feat(auth): update registration types and interfaces

--- mobile-app/sapot-mobile-app/features/auth/types.ts
+++ mobile-app/sapot-mobile-app/features/auth/types.ts
@@ -1,14 +1,24 @@
-export interface UserCredentials {
-  id?: string;
-  password: string;
+/**
+ * Register types and interfaces
+ */
+
+export interface RegisterFormState {
+  username: string;
   firstName: string;
   lastName: string;
   phoneNumber: string;
   email: string;
+  password: string;
+  confirmPassword: string;
+  termsChecked: boolean;
 }
+export type RegisterFormStateErrors = Partial<
+  Record<keyof RegisterFormState, string>
+> & { general?: string };
 
-export interface RegisterRequest {
+export interface RegisterApiRequest {
   id?: string;
+  username: string;
   password: string;
   first_name: string;
   last_name: string;
@@ -16,10 +26,31 @@ export interface RegisterRequest {
   email: string;
 }
 
-export interface AuthResponse {
+type ApiRegisterFieldErrorResponse = Partial<
+  Record<keyof RegisterApiRequest, string>
+>;
+
+interface ApiValidationErrorResponse {
+  type: string;
+  loc: string[];
+  msg: string;
+  input: unknown;
+  ctx?: Record<string, unknown>;
+}
+
+export interface RegisterErrorResponse {
+  // Used union type because of server response format is inconsistent
+  detail: ApiValidationErrorResponse[] | ApiRegisterFieldErrorResponse;
+}
+
+interface RegisterApiSuccessResponse {
   id: string;
   first_name: string;
   last_name: string;
   phone_number: string;
   email: string;
+  username: string;
+}
+export interface RegisterResponse {
+  data: RegisterApiSuccessResponse;
 }
