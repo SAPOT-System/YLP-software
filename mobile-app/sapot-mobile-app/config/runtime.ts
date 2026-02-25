e064c687f47428a8760b5bf87cacfe89ffa197ae
Adamskiee <tosinomatthew@gmail.com>
2026-02-25 14:21:09 +0800

    feat(runtime): update API URL handling and improve environment checks

--- mobile-app/sapot-mobile-app/config/runtime.ts
+++ mobile-app/sapot-mobile-app/config/runtime.ts
@@ -1,19 +1,20 @@
 import * as Updates from "expo-updates";
 
-interface ManifestExtra {
-  apiUrl: string;
-}
-
-type CustomManifest = Updates.Manifest & {
-  extra?: ManifestExtra;
-};
-
 export const getApiUrl = () => {
-  if (process.env.NODE_ENV === "development") {
+  if (__DEV__) {
     return "http://10.0.2.2:8000";
   }
 
-  // TODO: needs build to work
-  const manifest = Updates.manifest as CustomManifest;
-  return manifest?.extra?.apiUrl;
+  const channel = Updates.channel;
+
+  switch (channel) {
+    case "preview":
+      return "https://ylp-software.onrender.com";
+
+    case "production":
+      return "https://ylp-software.onrender.com";
+
+    default:
+      return "http://10.0.2.2:8000";
+  }
 };
