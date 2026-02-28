8181238b1b31c2ea3e9d99ce3bd3bdedd8a487b1
Adamskiee <tosinomatthew@gmail.com>
2026-02-28 10:29:39 +0800

    fix(auth-register): fix text field focus and mobile keyboard issue

--- mobile-app/sapot-mobile-app/app/getting-started/register.tsx
+++ mobile-app/sapot-mobile-app/app/getting-started/register.tsx
@@ -3,7 +3,13 @@ import { RegisterFormState } from "@/features/auth/types";
 import { ScreenContent, ScreenHeader } from "@/features/getting-started";
 import React, { useEffect, useState } from "react";
 import { Link, useRouter } from "expo-router";
-import { ScrollView, StyleSheet, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  View,
+} from "react-native";
 import {
   Button,
   Checkbox,
@@ -68,204 +74,212 @@ const Register = () => {
   };
 
   return (
-    <View
-      style={{ flex: 1, alignItems: "center", justifyContent: "flex-start" }}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
     >
-      <ScreenHeader headerName="Register Account" />
-      <ScreenContent
-        title="Welcome to SAPOT"
-        description="Create an account to get started"
+      <View
+        style={{ flex: 1, alignItems: "center", justifyContent: "flex-start" }}
       >
-        <ScrollView
-          keyboardShouldPersistTaps="handled"
-          style={{ width: "100%" }}
+        <ScreenHeader headerName="Register Account" />
+        <ScreenContent
+          title="Welcome to SAPOT"
+          description="Create an account to get started"
         >
-          <View style={{ alignItems: "stretch", marginBottom: 20 }}>
-            {/* */}
-            <TextInput
-              mode="outlined"
-              label="Username"
-              placeholder="Username"
-              value={form.username}
-              onChangeText={(value) => handleChange("username", value)}
-              style={styles.textInput}
-              error={!!errors.username}
-            />
-            {errors.username && (
-              <HelperText type="error" style={styles.helperText}>
-                {errors.username}
-              </HelperText>
-            )}
-
-            {/* First Name */}
-            <TextInput
-              mode="outlined"
-              label="First Name"
-              placeholder="First Name"
-              value={form.firstName}
-              onChangeText={(value) => handleChange("firstName", value)}
-              style={styles.textInput}
-              error={!!errors.firstName}
-            />
-            {errors.firstName && (
-              <HelperText type="error" style={styles.helperText}>
-                {errors.firstName}
-              </HelperText>
-            )}
-
-            {/* Last Name */}
-            <TextInput
-              mode="outlined"
-              label="Last Name"
-              placeholder="Last Name"
-              value={form.lastName}
-              onChangeText={(value) => handleChange("lastName", value)}
-              style={styles.textInput}
-              error={!!errors.lastName}
-            />
-            {errors.lastName && (
-              <HelperText type="error" style={styles.helperText}>
-                {errors.lastName}
-              </HelperText>
-            )}
-
-            {/* Phone Number */}
-            <TextInput
-              mode="outlined"
-              label="Phone Number"
-              placeholder="Phone Number"
-              value={form.phoneNumber}
-              onChangeText={(value) => handleChange("phoneNumber", value)}
-              keyboardType="phone-pad"
-              style={styles.textInput}
-              error={!!errors.phoneNumber}
-            />
-            {errors.phoneNumber && (
-              <HelperText type="error" style={styles.helperText}>
-                {errors.phoneNumber}
-              </HelperText>
-            )}
-
-            {/* Email Address */}
-            <TextInput
-              mode="outlined"
-              label="Email Address"
-              placeholder="Email Address"
-              value={form.email}
-              onChangeText={(value) => handleChange("email", value)}
-              keyboardType="email-address"
-              style={styles.textInput}
-              error={!!errors.email}
-            />
-            {errors.email && (
-              <HelperText type="error" style={styles.helperText}>
-                {errors.email}
-              </HelperText>
-            )}
-
-            {/* Password */}
-            <TextInput
-              mode="outlined"
-              label="Password"
-              placeholder="Password"
-              value={form.password}
-              onChangeText={(value) => handleChange("password", value)}
-              secureTextEntry
-              style={styles.textInput}
-              error={!!errors.password}
-            />
-            {errors.password && (
-              <HelperText type="error" style={styles.helperText}>
-                {errors.password}
-              </HelperText>
-            )}
-
-            {/* Confirm Password */}
-            <TextInput
-              mode="outlined"
-              label="Confirm Password"
-              placeholder="Confirm Password"
-              value={form.confirmPassword}
-              onChangeText={(value) => handleChange("confirmPassword", value)}
-              secureTextEntry
-              style={styles.textInput}
-              error={!!errors.confirmPassword}
-            />
-            {errors.confirmPassword && (
-              <HelperText type="error" style={styles.helperText}>
-                {errors.confirmPassword}
-              </HelperText>
-            )}
-
-            {/* Terms & Conditions */}
-            <View style={{ flexDirection: "row", alignItems: "center" }}>
-              <Checkbox
-                status={form.termsChecked ? "checked" : "unchecked"}
-                onPress={() => {
-                  handleChange("termsChecked", !form.termsChecked);
-                }}
+          <ScrollView
+            keyboardShouldPersistTaps="handled"
+            style={{ width: "100%" }}
+          >
+            <View style={{ alignItems: "stretch", marginBottom: 20 }}>
+              {/* */}
+              <TextInput
+                mode="outlined"
+                label="Username"
+                placeholder="Username"
+                value={form.username}
+                onChangeText={(value) => handleChange("username", value)}
+                style={styles.textInput}
+                error={!!errors.username}
               />
+              {errors.username && (
+                <HelperText type="error" style={styles.helperText}>
+                  {errors.username}
+                </HelperText>
+              )}
+
+              {/* First Name */}
+              <TextInput
+                mode="outlined"
+                label="First Name"
+                placeholder="First Name"
+                value={form.firstName}
+                onChangeText={(value) => handleChange("firstName", value)}
+                style={styles.textInput}
+                error={!!errors.firstName}
+              />
+              {errors.firstName && (
+                <HelperText type="error" style={styles.helperText}>
+                  {errors.firstName}
+                </HelperText>
+              )}
+
+              {/* Last Name */}
+              <TextInput
+                mode="outlined"
+                label="Last Name"
+                placeholder="Last Name"
+                value={form.lastName}
+                onChangeText={(value) => handleChange("lastName", value)}
+                style={styles.textInput}
+                error={!!errors.lastName}
+              />
+              {errors.lastName && (
+                <HelperText type="error" style={styles.helperText}>
+                  {errors.lastName}
+                </HelperText>
+              )}
+
+              {/* Phone Number */}
+              <TextInput
+                mode="outlined"
+                label="Phone Number"
+                placeholder="Phone Number"
+                value={form.phoneNumber}
+                onChangeText={(value) => handleChange("phoneNumber", value)}
+                keyboardType="phone-pad"
+                style={styles.textInput}
+                error={!!errors.phoneNumber}
+              />
+              {errors.phoneNumber && (
+                <HelperText type="error" style={styles.helperText}>
+                  {errors.phoneNumber}
+                </HelperText>
+              )}
+
+              {/* Email Address */}
+              <TextInput
+                mode="outlined"
+                label="Email Address"
+                placeholder="Email Address"
+                value={form.email}
+                onChangeText={(value) => handleChange("email", value)}
+                keyboardType="email-address"
+                style={styles.textInput}
+                error={!!errors.email}
+              />
+              {errors.email && (
+                <HelperText type="error" style={styles.helperText}>
+                  {errors.email}
+                </HelperText>
+              )}
+
+              {/* Password */}
+              <TextInput
+                mode="outlined"
+                label="Password"
+                placeholder="Password"
+                value={form.password}
+                onChangeText={(value) => handleChange("password", value)}
+                secureTextEntry
+                style={styles.textInput}
+                error={!!errors.password}
+              />
+              {errors.password && (
+                <HelperText type="error" style={styles.helperText}>
+                  {errors.password}
+                </HelperText>
+              )}
+
+              {/* Confirm Password */}
+              <TextInput
+                mode="outlined"
+                label="Confirm Password"
+                placeholder="Confirm Password"
+                value={form.confirmPassword}
+                onChangeText={(value) => handleChange("confirmPassword", value)}
+                secureTextEntry
+                style={styles.textInput}
+                error={!!errors.confirmPassword}
+              />
+              {errors.confirmPassword && (
+                <HelperText type="error" style={styles.helperText}>
+                  {errors.confirmPassword}
+                </HelperText>
+              )}
+
+              {/* Terms & Conditions */}
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <Checkbox
+                  status={form.termsChecked ? "checked" : "unchecked"}
+                  onPress={() => {
+                    handleChange("termsChecked", !form.termsChecked);
+                  }}
+                />
+                <Text
+                  variant="bodyMedium"
+                  style={{ color: theme.colors.onPrimaryContainer }}
+                >
+                  I agree to{" "}
+                  {/* TODO: Make this a link where it will show the terms and condition texts */}
+                  <Text
+                    variant="bodyMedium"
+                    style={{
+                      fontWeight: "bold",
+                      textDecorationLine: "underline",
+                    }}
+                  >
+                    Terms & Conditions
+                  </Text>
+                </Text>
+              </View>
+              {errors.termsChecked && (
+                <HelperText type="error" style={styles.helperText}>
+                  {errors.termsChecked}
+                </HelperText>
+              )}
+            </View>
+            <View style={{ alignItems: "center" }}>
+              {/* Submit Button */}
+              <Button
+                onPress={() => handleRegister()}
+                mode="contained"
+                style={{ width: 280, marginBottom: 8 }}
+                loading={loading}
+                disabled={loading}
+              >
+                Create Account
+              </Button>
+
+              {/* Login Link */}
               <Text
                 variant="bodyMedium"
                 style={{ color: theme.colors.onPrimaryContainer }}
               >
-                I agree to{" "}
-                {/* TODO: Make this a link where it will show the terms and condition texts */}
-                <Text
-                  variant="bodyMedium"
+                Already have an account?{" "}
+                <Link
+                  href="/getting-started/server-login"
                   style={{
                     fontWeight: "bold",
                     textDecorationLine: "underline",
                   }}
                 >
-                  Terms & Conditions
-                </Text>
+                  Login Here
+                </Link>
               </Text>
             </View>
-            {errors.termsChecked && (
-              <HelperText type="error" style={styles.helperText}>
-                {errors.termsChecked}
-              </HelperText>
-            )}
-          </View>
-          <View style={{ alignItems: "center" }}>
-            {/* Submit Button */}
-            <Button
-              onPress={() => handleRegister()}
-              mode="contained"
-              style={{ width: 280, marginBottom: 8 }}
-              loading={loading}
-              disabled={loading}
-            >
-              Create Account
-            </Button>
+          </ScrollView>
+        </ScreenContent>
 
-            {/* Login Link */}
-            <Text
-              variant="bodyMedium"
-              style={{ color: theme.colors.onPrimaryContainer }}
-            >
-              Already have an account?{" "}
-              <Link
-                href="/getting-started/server-login"
-                style={{ fontWeight: "bold", textDecorationLine: "underline" }}
-              >
-                Login Here
-              </Link>
-            </Text>
-          </View>
-        </ScrollView>
-      </ScreenContent>
-
-      {/* Toast Notification */}
-      <Snackbar
-        visible={toastVisible}
-        onDismiss={() => setToastVisible(false)}
-        duration={3000}
-      >
-        {toastMessage}
-      </Snackbar>
-    </View>
+        {/* Toast Notification */}
+        <Snackbar
+          visible={toastVisible}
+          onDismiss={() => setToastVisible(false)}
+          duration={3000}
+        >
+          {toastMessage}
+        </Snackbar>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
