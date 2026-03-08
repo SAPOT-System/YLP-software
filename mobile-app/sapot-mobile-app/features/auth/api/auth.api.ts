c71a521e616ce1992b0047128deadd58d5a533ba
Adamskiee <tosinomatthew@gmail.com>
2026-03-08 11:27:03 +0800

    feat(auth-forgot-password): Add recovery key verification API and related types

--- mobile-app/sapot-mobile-app/features/auth/api/auth.api.ts
+++ mobile-app/sapot-mobile-app/features/auth/api/auth.api.ts
@@ -1,5 +1,5 @@
-import { AxiosResponse } from "axios";
 import { apiClient } from "@/features/shared";
+import { AxiosResponse } from "axios";
 import {
   LoginApiRequest,
   LoginApiResponse,
@@ -89,12 +89,45 @@ export const canResetPasswordApi = async (token: string) => {
 };
 
 export const resetPasswordApi = async (token: string, newPassword: string) => {
-
   const res = await apiClient.post<{ message: string }>(
     "/auth/forgot-password/reset-password",
     { new_password: newPassword },
     { params: { token } }
   );
 
+  return res;
+};
+
+export type ExpoFileUpload = {
+  uri: string;
+  name: string;
+  type: string;
+};
+
+export const verifyRecoveryKeyApi = async (
+  file: ExpoFileUpload,
+  identifier: string
+) => {
+  // eslint-disable-next-line no-undef
+  const formData = new FormData();
+  console.log(identifier);
+  formData.append("key_file", {
+    uri: file.uri,
+    name: file.name,
+    type: file.type,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } as any);
+
+  const res = await apiClient.post<{
+    "recovery-link": string;
+    method: string;
+    expire_in_seconds: number;
+  }>("/auth/forgot-password/recovery-with-recovery-key", formData, {
+    params: { user_identifier: identifier },
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Accept: "application/json",
+    },
+  });
   return res;
 };
