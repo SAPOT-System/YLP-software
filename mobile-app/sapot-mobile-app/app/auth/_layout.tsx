31df5480de2760725d412f0da15c8350e3384c79
Adamskiee <tosinomatthew@gmail.com>
2026-03-08 14:41:49 +0800

    refactor(auth): simplify stack navigation by consolidating screen options

--- mobile-app/sapot-mobile-app/app/auth/_layout.tsx
+++ mobile-app/sapot-mobile-app/app/auth/_layout.tsx
@@ -6,22 +6,7 @@ export default function Layout() {
   return (
     <SafeAreaView style={{ flex: 1 }} edges={["left", "right", "bottom"]}>
       <HealthProvider>
-        <Stack>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="lan-login" options={{ headerShown: false }} />
-          <Stack.Screen name="server-login" options={{ headerShown: false }} />
-          <Stack.Screen name="register" options={{ headerShown: false }} />
-          <Stack.Screen
-            name="forgot-password"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen name="email-reset" options={{ headerShown: false }} />
-          <Stack.Screen name="sms-reset" options={{ headerShown: false }} />
-          <Stack.Screen
-            name="enter-recovery"
-            options={{ headerShown: false }}
-          />
-        </Stack>
+        <Stack screenOptions={{ headerShown: false }} />
       </HealthProvider>
     </SafeAreaView>
   );
