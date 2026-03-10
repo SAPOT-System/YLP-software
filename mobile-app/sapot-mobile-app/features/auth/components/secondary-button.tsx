cc81eb102053d3ce14e16c42f96d6418ac0fbc0c
Adamskiee <tosinomatthew@gmail.com>
2026-03-10 08:18:41 +0800

    feat(auth): add primary and secondary button components for authentication

---
+++ mobile-app/sapot-mobile-app/features/auth/components/secondary-button.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { StyleProp, StyleSheet, ViewStyle } from "react-native";
+import { Button, ButtonProps } from "react-native-paper";
+
+interface SecondaryButtonProps extends ButtonProps {
+  children: React.ReactNode;
+  onPress: () => void;
+  loading?: boolean;
+  disabled?: boolean;
+  style?: StyleProp<ViewStyle>;
+}
+
+const SecondaryButton = ({
+  children,
+  style,
+  loading,
+  disabled,
+  onPress,
+  ...props
+}: SecondaryButtonProps) => {
+  return (
+    <Button
+      onPress={onPress}
+      mode="outlined"
+      loading={loading}
+      disabled={disabled}
+      style={[styles.button, style]}
+      {...props}
+    >
+      {children}
+    </Button>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    width: 280,
+    height: 52,
+    borderRadius: 30,
+    justifyContent: "center",
+  },
+});
+
+export default SecondaryButton;
