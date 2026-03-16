f4f4ff2cd6fe309146beb282ffe8aca17dfafca7
Emman-pip <parrenoemmanuel756@gmail.com>
2026-03-16 15:46:27 +0800

    feature(api-sync) Create database models for syncing.
    Mainly conversation models and changes in user model.

--- server/app/models/conversation.py
+++ server/app/models/conversation.py
@@ -2,16 +2,25 @@
 
 import datetime
 from enum import Enum, auto
-from typing import Annotated, Literal
+from typing import Annotated, List, Literal, Optional
 from uuid import UUID, uuid4
-from sqlmodel import Field, SQLModel, table
+from sqlmodel import Field, Relationship, SQLModel, UniqueConstraint, table
+from datetime import timezone
 
+class ConversationType(str, Enum):
+    DIRECT = "direct_message"
+    GROUP = "group"
+    SOLO = "solo"
 
 class ConversationBase(SQLModel):
     title : str = Field(max_length=100, min_length=2)
-    conversation_type : Literal['direct_message', 'group', 'solo'] = Field(max_length=100, min_length=2)
+    conversation_type : ConversationType = Field(max_length=100, min_length=2)
     created_at : datetime.datetime = Field(default_factory=datetime.datetime.now)
 
+    conversation_participants: List["ConversationParticipant"] = Relationship(back_populates="conversation")
+    messages: List["Message"] = Relationship(back_populates="conversation")
+    calls: List["Call"] = Relationship(back_populates="conversation")
+    call_participants: List["CallParticipant"] = Relationship(back_populates="conversation")
 
 class Conversation(ConversationBase, table=True):
     id: UUID | None = Field(default_factory=uuid4, unique=True, primary_key=True, index=True)
@@ -22,16 +31,24 @@ class ConversationPublic(ConversationBase):
 
 
 class ConversationParticipantBase(SQLModel):
-    role : Literal["initiator", "participant"]
-    joined_at : datetime.datetime = Field(default_factory=datetime.datetime.now)
+    # role is redundant, the purpose of this is to track if the user is a participant or not
+    # role : Literal["initiator", "participant"]
+    joined_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     is_deleted : bool = False
     # foreign keys
-    user_id: UUID | None = Field(default=None, index=True, foreign_key='user.id')
     conversation_id: UUID | None = Field(default=None, index=True, foreign_key='conversation.id')
+    user_id: UUID = Field(default=None, index=True, foreign_key='user.id')
+
+    user: List['User'] = Relationship(back_populates="conversation_participants")
+    conversation: List['Conversation'] = Relationship(back_populates="conversation_participants")
+
+    __table_args__ = (
+        UniqueConstraint("conversation_id", "user_id"),
+    )
 
 
 class  ConversationParticipant(ConversationParticipantBase, table=True):
-    id: UUID | None = Field(default_factory=uuid4, unique=True, index=True, primary_key=True)
+    id: UUID | None = Field(default_factory=uuid4, index=True, primary_key=True)
 
 
 class MessageBase(SQLModel):
@@ -43,6 +60,11 @@ class MessageBase(SQLModel):
     # foreign_key
     conversation_id : UUID | None = Field(default=None, foreign_key='conversation.id')
     sender_id : UUID | None = Field(default=None, foreign_key='user.id')
+
+    user: Optional['User'] = Relationship(back_populates="messages")
+    conversation: Optional['Conversation'] = Relationship(back_populates="messages")
+    message_receipt: Optional['MessageReceipt'] =  Relationship(back_populates="messages")
+    attachment: Optional['Attachment'] =  Relationship(back_populates="messages")
 
 
 class Message(MessageBase, table=True):
@@ -59,8 +81,11 @@ class CallBase(SQLModel):
     conversation_id : UUID | None = Field(default=None, foreign_key='conversation.id')
     initiator_id : UUID  | None = Field(default=None, foreign_key='user.id')
 
+    user: List['User'] = Relationship(back_populates="calls")
+    conversation: Optional['Conversation'] = Relationship(back_populates="calls")
 
-class Call(MessageBase, table=True):
+
+class Call(CallBase, table=True):
     id : UUID | None = Field(
         default_factory=uuid4,
         primary_key=True,
@@ -76,8 +101,12 @@ class CallParticipantBase(SQLModel):
     conversation_id : UUID | None = Field(foreign_key='conversation.id')
     user_id : UUID | None = Field(foreign_key='user.id')
 
+    user: List['User'] = Relationship(back_populates="call_participants")
+    conversation: Optional['Conversation'] = Relationship(back_populates="call_participants")
 
-class CallParticipant(CallParticipantBase):
+
+
+class CallParticipant(CallParticipantBase, table=True):
     id : UUID | None = Field(default_factory=uuid4, primary_key=True, index=True)
 
 
@@ -88,6 +117,10 @@ class MessageReceiptBase(SQLModel):
     # foreign keys
     message_id : UUID = Field(foreign_key="message.id")
     user_id : UUID = Field(foreign_key="user.id")
+
+    user: Optional['User'] = Relationship(back_populates="message_receipts")
+    message: Optional['Message'] = Relationship(back_populates="message_receipts")
+
 
 
 class MessageReceipt(MessageReceiptBase, table=True):
@@ -100,7 +133,9 @@ class AttachmentBase(SQLModel):
     file_name : str = Field(max_length=200, min_length=1)
     file_size : int
     mime_type : str = Field(max_length=255, min_length=1)
-}
+
+    message: Optional['Message'] = Relationship(back_populates="attachments")
+
 
 class Attachment(AttachmentBase, table=True):
     id : UUID | None = Field(default_factory=uuid4, index=True, primary_key=True)
