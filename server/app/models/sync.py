0d5b30822bd0331f3371e230731ef2f5bfc173a4
Emman-pip <parrenoemmanuel756@gmail.com>
2026-03-29 16:51:27 +0800

    feature(api-sync) Create the push-sync function.
    Let clients push sync data.

--- server/app/models/sync.py
+++ server/app/models/sync.py
@@ -10,8 +10,13 @@ from typing import TYPE_CHECKING
 from app.models.message_receipt import MessageReceipt
 from app.models.message import Message
 from app.models.call import Call
-from app.models.conversation import Conversation
+from app.models.conversation import Conversation, ConversationParticipant
 from app.models.call_participant import CallParticipant
+
+class SyncCheckResponse(BaseModel):
+    has_updates: bool
+    new_items_count: int
+    last_update_at: Optional[datetime] = None
 
 # This schema is what the client sends
 class SyncRequest(BaseModel):
@@ -29,5 +34,14 @@ class SyncResponse(BaseModel):
     new_cursor: datetime
     has_more: bool = False
 
-SyncResponse.model_rebuild()
-SyncRequest.model_rebuild()
+
+class PushSyncRequest(BaseModel):
+    conversations: List['Conversation'] = []
+    messages: List['Message']  = []
+    conversation_participants: List['ConversationParticipant']  = []
+    calls: List['Call']  = []
+    call_participants: List['CallParticipant']  = []
+    message_receipts: List['MessageReceipt'] = []
+#
+# SyncResponse.model_rebuild()
+# SyncRequest.model_rebuild()
