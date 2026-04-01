28f68619e4f8d70f952b90af2d07829b45e2de93
Emman-pip <parrenoemmanuel756@gmail.com>
2026-04-01 16:38:34 +0800

    feature(api-gps) Model the location and rescuers.

--- server/app/models/users.py
+++ server/app/models/users.py
@@ -16,6 +16,7 @@ if TYPE_CHECKING:
     from app.models.message import Message
     from app.models.user_profile_picture import UserProfilePicture
     from app.models.message_receipt import MessageReceipt
+    from app.models.location import UserLocation
 
 
 PhoneStr = Annotated[
@@ -70,6 +71,15 @@ class User(UserBase, table=True):
         back_populates="user"
     )
 
+    locations: list["UserLocation"] = Relationship(
+        back_populates="user",
+        sa_relationship_kwargs={"cascade": "all, delete-orphan"}
+    )
+
+    rescuer: Optional["UserLocation"] = Relationship(
+        back_populates="user",
+        sa_relationship_kwargs={"cascade": "all, delete-orphan"}
+    )
 
 class UserPublic(UserBase):
     id: uuid.UUID
