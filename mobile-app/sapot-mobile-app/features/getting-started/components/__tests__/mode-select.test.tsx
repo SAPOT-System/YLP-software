dea761513d892f28b8ab11e518280f16e06e82dd
Adamskiee <tosinomatthew@gmail.com>
2026-05-01 18:05:25 +0800

    test(getting-started): add mocks for react-native-paper to improve test rendering

--- mobile-app/sapot-mobile-app/features/getting-started/components/__tests__/mode-select.test.tsx
+++ mobile-app/sapot-mobile-app/features/getting-started/components/__tests__/mode-select.test.tsx
@@ -1,5 +1,44 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { fireEvent, render } from "@testing-library/react-native";
 import React from "react";
+
+// Provide test-friendly mocks for react-native-paper so ModeSelect can render
+jest.mock("react-native-paper", () => {
+  const React = require("react");
+  const RN = require("react-native");
+
+  const Text = (props: any) => React.createElement(RN.Text, props, props.children);
+  const Button = (props: any) => React.createElement(RN.TouchableOpacity, props, props.children);
+  const Icon = (_props: any) => React.createElement(RN.View, null, null);
+  const IconButton = (props: any) => React.createElement(RN.TouchableOpacity, props, props.children);
+  const Portal = ({ children }: { children?: React.ReactNode }) =>
+    React.createElement(React.Fragment, null, children);
+  const Modal = (props: any) => React.createElement(RN.View, props, props.children);
+
+  const TextInput = (props: any) => React.createElement(RN.TextInput, { ...props });
+  TextInput.Icon = (_props: any) => React.createElement(RN.View, null, null);
+
+  return {
+    __esModule: true,
+    Text,
+    Button,
+    Icon,
+    IconButton,
+    Portal,
+    Modal,
+    TextInput,
+    useTheme: () => ({
+      colors: {
+        primary: "#000",
+        onPrimaryContainer: "#ccc",
+        primaryContainer: "#eee",
+        inverseOnSurface: "#000",
+        surface: "#fff",
+      },
+    }),
+  };
+});
+
 import { ModeSelect } from "../mode-select";
 
 describe("ModeSelect", () => {
