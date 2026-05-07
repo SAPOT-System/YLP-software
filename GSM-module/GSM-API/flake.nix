ce3d351d884acb3c33547bd178b6c8e01db043e6
Emman-pip <parrenoemmanuel756@gmail.com>
2026-05-07 21:45:12 +0800

    feature(GSM) Delete flake.lock.

--- GSM-module/GSM-API/flake.nix
+++ GSM-module/GSM-API/flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
-  outputs = { self, nixpkgs, android-nixpkgs }:
+  outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
