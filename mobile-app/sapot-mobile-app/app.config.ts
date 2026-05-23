7e705547d77a97bd2fd5c6b5c8fb6c8b9bd996ca
Adamskiee <tosinomatthew@gmail.com>
2026-05-23 10:21:40 +0800

    chore: downgrade @sentry/react-native from 8.12.0 to 7.2.0 for compatibility

--- mobile-app/sapot-mobile-app/app.config.ts
+++ mobile-app/sapot-mobile-app/app.config.ts
@@ -183,6 +183,7 @@ export default ({ config }: ConfigContext) => ({
         barcodeScannerEnabled: true,
       },
     ],
+    "@sentry/react-native",
     [
       "@sentry/react-native/expo",
       {
