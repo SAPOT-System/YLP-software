de61eeb4d99f51918180befb9ff2e0b1a226e04d
Adamskiee <tosinomatthew@gmail.com>
2026-05-30 16:30:52 +0800

    fix(eas): update display version to 0.4.1

--- mobile-app/sapot-mobile-app/app.config.ts
+++ mobile-app/sapot-mobile-app/app.config.ts
@@ -232,6 +232,6 @@ export default ({ config }: ConfigContext) => ({
       projectId: "ee940ed5-5653-43cb-8938-d5f54a830c59",
     },
     apiUrl: process.env.EXPO_PUBLIC_API_URL,
-    displayVersion: "0.3.0",
+    displayVersion: "0.4.1",
   },
 });
