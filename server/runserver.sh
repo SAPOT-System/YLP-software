fee3746e64d40d7495db3485234d7d4bbd95e6d7
Adamskiee <tosinomatthew@gmail.com>
2026-06-04 12:10:27 +0800

    fix(server): add crt to gitignore, guard missing certs, clarify IP in docs

--- server/runserver.sh
+++ server/runserver.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+[ -f /home/sapot/certs/server.key ] || { echo "ERROR: TLS key not found at /home/sapot/certs/server.key"; exit 1; }
+[ -f /home/sapot/certs/server.crt ] || { echo "ERROR: TLS cert not found at /home/sapot/certs/server.crt"; exit 1; }
+
 source /home/sapot/YLP-software/server/app/venv/bin/activate
 pip install -r /home/sapot/YLP-software/server/app/requirements.txt
 /home/sapot/YLP-software/server/app/venv/bin/gunicorn app.main:app \
