afe257fc6130e92c16b15af6b25f8b724819fbd4
Adamskiee <tosinomatthew@gmail.com>
2026-06-04 12:06:18 +0800

    feat(server): enable TLS on gunicorn with self-signed cert

--- server/runserver.sh
+++ server/runserver.sh
@@ -2,4 +2,9 @@
 
 source /home/sapot/YLP-software/server/app/venv/bin/activate
 pip install -r /home/sapot/YLP-software/server/app/requirements.txt
-/home/sapot/YLP-software/server/app/venv/bin/gunicorn app.main:app   -k uvicorn.workers.UvicornWorker   -w 4   -b 0.0.0.0:8000 
+/home/sapot/YLP-software/server/app/venv/bin/gunicorn app.main:app \
+  -k uvicorn.workers.UvicornWorker \
+  -w 4 \
+  -b 0.0.0.0:8000 \
+  --keyfile /home/sapot/certs/server.key \
+  --certfile /home/sapot/certs/server.crt
