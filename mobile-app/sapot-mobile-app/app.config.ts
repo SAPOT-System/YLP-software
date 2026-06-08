0355be5f5d3b8f4aabce276f3ee90bb149145f77
Adamskiee <tosinomatthew@gmail.com>
2026-06-08 15:54:06 +0800

    chore(version): update displayVersion to 0.5.2

--- mobile-app/sapot-mobile-app/app.config.ts
+++ mobile-app/sapot-mobile-app/app.config.ts
@@ -298,6 +298,6 @@ export default ({ config }: ConfigContext) => ({
       projectId: "ee940ed5-5653-43cb-8938-d5f54a830c59",
     },
     apiUrl: process.env.EXPO_PUBLIC_API_URL,
-    displayVersion: "0.5.1",
+    displayVersion: "0.5.2",
   },
 });
