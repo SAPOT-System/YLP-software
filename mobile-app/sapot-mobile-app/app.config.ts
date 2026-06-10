cece12793f9baa31e8a4f9656840ab04c6d945f2
Adamskiee <tosinomatthew@gmail.com>
2026-06-10 13:03:35 +0800

    chore(version): update version number to 0.6.0

--- mobile-app/sapot-mobile-app/app.config.ts
+++ mobile-app/sapot-mobile-app/app.config.ts
@@ -158,7 +158,7 @@ export default ({ config }: ConfigContext) => ({
   ...config,
   name: getAppName(),
   slug: "sapot-mobile-app",
-  version: "0.5.0",
+  version: "0.6.0",
   orientation: "portrait",
   icon: "./assets/images/logo.png",
   scheme: "sapotmobileapp",
@@ -298,6 +298,6 @@ export default ({ config }: ConfigContext) => ({
       projectId: "ee940ed5-5653-43cb-8938-d5f54a830c59",
     },
     apiUrl: process.env.EXPO_PUBLIC_API_URL,
-    displayVersion: "0.5.2",
+    displayVersion: "0.6.0",
   },
 });
