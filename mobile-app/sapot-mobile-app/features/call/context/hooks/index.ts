5f75a82c27ab85d12b085eb0edee00f33ec443d5
Adamskiee <tosinomatthew@gmail.com>
2026-06-24 19:55:03 +0800

    refactor(call): extract useCallLifecycle state machine hook

---
+++ mobile-app/sapot-mobile-app/features/call/context/hooks/index.ts
@@ -0,0 +1,7 @@
+export { useCallLifecycle, type CallState } from "./use-call-lifecycle";
+export { useCallTimer } from "./use-call-timer";
+export { useAudioRoute } from "./use-audio-route";
+export { usePeerInfo } from "./use-peer-info";
+export { useRemoteStream } from "./use-remote-stream";
+export { useLocalStream } from "./use-local-stream";
+export { useCallMediaState } from "./use-call-media-state";
