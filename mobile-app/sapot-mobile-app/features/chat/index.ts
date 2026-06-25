4a65edd98252c1d803cd1e7d0cf2bb9a1054a014
Adamskiee <tosinomatthew@gmail.com>
2026-06-25 21:30:38 +0800

    refactor(chat): rename component/ to components/ for naming consistency

--- mobile-app/sapot-mobile-app/features/chat/index.ts
+++ mobile-app/sapot-mobile-app/features/chat/index.ts
@@ -1,7 +1,7 @@
 import { chatLog } from "@/features/shared/utils/logger";
 chatLog.debug("[chat/index] module loaded");
 
-export * from "./component";
+export * from "./components";
 
 export * from "./hooks";
 
